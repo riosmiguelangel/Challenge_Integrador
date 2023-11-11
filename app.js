fced8b8497c0b7e8f67b0f850aad4342c94bbd01
Miguel Angel Rios <riosmiguelangel@yahoo.com.ar>
2023-11-11 08:55:03 -0300

    migrando a servidor dinamico. Creacion carpeta public y alojar archivos estaticos en ella, creacion de capeta src y subcarpetas routes y src, modificacion endpoint app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const mainRoutes = require('./src/routes/mainRoutes');
+const shopRoutes = require('./src/routes/shopRoutes');
+const adminRoutes = require('./src/routes/adminRoutes');
+const authRoutes = require('./src/routes/authRoutes');
 
 app.use(express.static('public'));
-
-app.get('/ping', (req, res) => res.send('pong'));
+app.use('/', mainRoutes);
+app.use('/shop', shopRoutes);
+app.use('/admin', adminRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000") ); 
