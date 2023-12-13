63a95e014652af66b2ff53c1d34ce2ae34bfc3e8
Miguel Angel Rios <riosmiguelangel@yahoo.com.ar>
2023-12-13 08:27:31 -0300

    modifico logueo

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -19,20 +19,22 @@ authLoginPost: async (req, res) => {
   }
   const [valido] = await userModel.verificarUser(userLogin.email, userLogin.password)
   if(valido === undefined){
+    console.log("undefined: ", valido)
     res.redirect('/auth/login')
   } else if(await (userLogin.email != valido.email) && (userLogin.password != valido.password)){
     //else if(!(await crypt.compare(userLogin.email, valido.email) && crypt.compare(password, valido.password))){
+      console.log("else: ", valido)
     console.log(userLogin.email, valido.email, userLogin.password, valido.password)
     res.redirect('../../login/?error=1')
   } else {
     req.session.user_id = valido.user_id
     console.log(req.session.user_id)
-    if(req.session.user_id ==2){
+    /*if(req.session.user_id ==2){
       console.log("usuario dos") 
       const logueado = true
       console.log(logueado)
     } else {
-      console.log(req.session.user_id)}
+      console.log(req.session.user_id)}*/
     //return logueado= req.session.user_id,
     //console.log("logueado: ",logueado),
     res.redirect(`../../home?user_id=${valido.user_id}`)
