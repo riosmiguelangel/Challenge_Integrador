fa7c5f85e550e0b45582f344c5d3ebdccda4b1e1
Miguel Angel Rios <riosmiguelangel@yahoo.com.ar>
2023-12-14 14:48:30 -0300

    modificacion en islogged en rutas

--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const shopControllers = require('../controllers/shopControllers');
 
 
-router.get('/', shopControllers.shopView);
-router.get('/item/:id', shopControllers.itemView);
-router.post('/item/:id/add', shopControllers.addItemToCart);
-router.get('/cart', shopControllers.cartView);
-router.post('/cart', shopControllers.checkout);
+router.get('/', isLogged, shopControllers.shopView);
+router.get('/item/:id', isLogged, shopControllers.itemView);
+router.post('/item/:id/add', isLogged, shopControllers.addItemToCart);
+router.get('/cart', isLogged, shopControllers.cartView);
+router.post('/cart', isLogged, shopControllers.checkout);
 
 module.exports = router;
